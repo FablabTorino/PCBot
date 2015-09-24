da3adfc68852db91887d6dd54790ecce0b6804da
lorenzo <l.romagnoli@arduino.cc>
2015-09-24 02:07:00 +0200

    deleted many of the stuff from Mariobot not usefull. used urllib.urlretrieve to download files - both for the json and for the images temporary data are stored in tempData folder
    !it should be flushed at some point
    Maybe at when we reboot the edison?

--- PCBot.py
+++ PCBot.py
@@ -8,18 +8,16 @@ import time
 import calendar
 import datetime
 from conversation import *
-import pywapi
 import string
-import wget
-import os
 import json
+import urllib
 
 import printer
 import privateData
 
 # Telegram Bot Authorization Token
 # SuperMario_bot
-bot = telegram.Bot(token)
+bot = telegram.Bot(privateData.token)
 #AUGChatId=str(-22985187)
 #AUGChatId=str(-26538515)
 AUGChatId=str(-6549559)
@@ -39,66 +37,16 @@ except IndexError:
    # reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
    # bot.sendMessage(chat_id=chat_id, text="Stay here, I'll be back.", reply_markup=reply_markup)
 
-#----------------------------------------------------------------------
-#  eliza.py
-#
-#  a cheezy little Eliza knock-off by Joe Strout <joe@strout.net>
-#  with some updates by Jeff Epler <jepler@inetnebr.com>
-#  hacked into a module and updated by Jez Higgins <jez@jezuk.co.uk>
-#  last revised: 28 February 2005
-#----------------------------------------------------------------------
 
 import string
 import re
 import random
 
-class eliza:
-  def __init__(self):
-    self.keys = map(lambda x:re.compile(x[0], re.IGNORECASE),gPats)
-    self.values = map(lambda x:x[1],gPats)
 
-  #----------------------------------------------------------------------
-  # translate: take a string, replace any words found in dict.keys()
-  #  with the corresponding dict.values()
-  #----------------------------------------------------------------------
-  def translate(self,str,dict):
-    words = string.split(string.lower(str))
-    keys = dict.keys();
-    for i in range(0,len(words)):
-      if words[i] in keys:
-        words[i] = dict[words[i]]
-    return string.join(words)
-
-  #----------------------------------------------------------------------
-  #  respond: take a string, a set of regexps, and a corresponding
-  #    set of response lists; find a match, and return a randomly
-  #    chosen response from the corresponding list.
-  #----------------------------------------------------------------------
-  def respond(self,str):
-    # find a match among keys
-    for i in range(0,len(self.keys)):
-      match = self.keys[i].match(str)
-      if match:
-        # found a match ... stuff with corresponding value
-        # chosen randomly from among the available options
-        resp = random.choice(self.values[i])
-        # we've got a response... stuff in reflected text where indicated
-        pos = string.find(resp,'%')
-        while pos > -1:
-          num = string.atoi(resp[pos+1:pos+2])
-          resp = resp[:pos] + \
-            self.translate(match.group(num),gReflections) + \
-            resp[pos+2:]
-          pos = string.find(resp,'%')
-        # fix munged punctuation at the end
-        if resp[-2:] == '?.': resp = resp[:-2] + '.'
-        if resp[-2:] == '??': resp = resp[:-2] + '?'
-        return resp
 
 
 def echo():
     global LAST_UPDATE_ID
-    therapist = eliza();
     # Request updates from last updated_id
     try:
         for update in bot.getUpdates(offset=LAST_UPDATE_ID):
@@ -115,12 +63,9 @@ def echo():
                 else: message=None
                 print(from_user)
                 if (message):
+                    #this stuff is not core of the printing breadboard layout stuff... just as reference
                     if '\AUGcalendar' in message:
                         AUGCalendar()
-                        LAST_UPDATE_ID = update.update_id
-                        return
-                    elif '\weather' in message:
-                        weather_info()
                         LAST_UPDATE_ID = update.update_id
                         return
                     elif '/breadboards_list' in message:
@@ -133,7 +78,7 @@ def echo():
                         print_breadboard(message, chat_id)
                         LAST_UPDATE_ID = update.update_id
                         return
-                    elif '/help' in message:
+                    elif '/help' in message: #please fix this
                         print 'attaccati al tram'
                         bot.sendMessage(chat_id=chat_id, text="Please help me! I'm stuck here! It's a me, Mario!")
                         LAST_UPDATE_ID = update.update_id
@@ -143,34 +88,7 @@ def echo():
                         bot.sendMessage(chat_id=chat_id, text="What is "+message+"?")
                         LAST_UPDATE_ID = update.update_id
                         return
-                if 'Giancarlo' in from_user['first_name']:
-                    if (message):
-                        if '\say' in message:
-                            message=message.partition(" ")
-                            type(message)
-                            bot.sendMessage(chat_id=AUGChatId, text=message[2])
-                            #bot.sendMessage(chat_id='-6581067', text=message[2])
-                            LAST_UPDATE_ID = update.update_id
-                            return
-                if 'Giancarlo' in from_user['first_name']:
-                    if (message):
-                        if '\calendar' in message:
-                            message=message.partition(" ")
-                            type(message)
-                            message=message[2].partition(" ")
-                            type(message)
-                            c = calendar.TextCalendar(calendar.SUNDAY)
-                            calen=c.formatmonth(int(message[0]),int(message[2]))
-                            calen=calen.replace("  ", "   ")
-                            calen=calen.replace(" ", " ")
-                            bot.sendMessage(chat_id=AUGChatId, text=calen)
-                            LAST_UPDATE_ID = update.update_id
-                            return
-                if 'Giancarlo' in from_user['first_name']:
-                    if update.message.photo!=None:
-                        bot.sendPhoto(chat_id=AUGChatId, photo=update.message.photo)
-                        LAST_UPDATE_ID = update.update_id
-                        return
+
                 if (message):
                     if '#print' in message:
                         bot.sendMessage(chat_id=chat_id, text=message)
@@ -218,47 +136,13 @@ def AUGCalendar():
     bot.sendMessage(chat_id=AUGChatId, text=message)
 
 
-
-def weather_info():
-    #!/usr/bin/env python
-    therapist = eliza();
-    from urllib2 import urlopen
-    from contextlib import closing
-    import json
-    now = datetime.datetime.now()
-    day=str(now.day)
-    year=str(now.year)
-    month=str(now.month)
-    today = str(datetime.datetime.today().weekday())
-    today=therapist.translate(today,gDayEn)
-    month=therapist.translate(month,gMonthIta)
-    weather_com_result = pywapi.get_weather_from_weather_com('ITPM3241')
-    print weather_com_result
-    weathernow=therapist.translate(string.lower(weather_com_result['current_conditions']['text']),gWeatherIta)
-    for i in weather_com_result['forecasts']:
-        if i['day_of_week']==today:
-            print i
-            dayfore= therapist.translate(string.lower(i['day']['text']),gWeatherIta)
-            nightfore=therapist.translate(string.lower(i['night']['text']),gWeatherIta)
-            maxtemp=string.lower(i['high'])
-            mintemp=string.lower(i['low'])
-            break
-    temp=str(weather_com_result['current_conditions']['temperature'])
-    today=therapist.translate(str(datetime.datetime.today().weekday()),gDayIta)
-    message= "Oggi é "+today+", "+day+" "+month+" "+year+"\n in questo momento sono le "+str(now.hour)+" e "+str(now.minute)+" minuti"
-    bot.sendMessage(chat_id=AUGChatId, text=message)
-    message= "tempo "+weathernow+", temperatura di "+temp+"C"
-    if now.hour<12:
-        message=message+"\n durante la giornata "+dayfore
-    message=message+"\n in serata "+nightfore
-    message=message+"\n temperatura massima: "+maxtemp+"C"
-    message=message+"\n temperatura minima: "+mintemp+"C"
-    bot.sendMessage(chat_id=AUGChatId, text=message)
-
 #--------------------------------#
 #lists available boards from a json file
 def breadboards_list(chat_id):
-    filename=wget.download(DataUrl)
+
+    filename='tempData/data.json'
+    urllib.urlretrieve(DataUrl, filename)
+
     print filename
 
     from pprint import pprint
@@ -268,23 +152,30 @@ def breadboards_list(chat_id):
         print i['name']
         bot.sendMessage(chat_id=chat_id, text="/breadboard_"+i['name'])
 
-    os.remove(filename)
 
 #--------------------------------#
 #prints images with the thermal printer
 def print_breadboard(message, chat_id):
 
-    filename=wget.download(DataUrl)
+    #filename=wget.download(DataUrl)
+
+    filename='tempdata/data.json'
+    urllib.urlretrieve(DataUrl, filename)
+
     with open(filename) as jsonFile :
         data = json.load(jsonFile)
 
-    os.remove(filename)
 
     for i in data['breadboard'] :
         if i['name'] in message :
             print i['filename']
             #os.remove(i['filename'])
-            filename=wget.download(GFXUrl+i['filename'])
+
+            filename='tempdata/'+i['filename']
+            url=GFXUrl+i['filename']
+            urllib.urlretrieve(url, filename)
+
+            #filename=wget.download(GFXUrl+i['filename'])
             bot.sendMessage(chat_id=chat_id, text="ok, printing "+message)
             #os.remove(filename)
             return
